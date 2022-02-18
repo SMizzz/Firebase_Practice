0d8ee17fd333c3ae4166f9f5a547d44a9360f019
SMizzz <shinn.mizzz@gmail.com>
2022-02-18 17:42:29 +0900

    Firebase Storage Setting.

--- Firebase_Practice/PhotoUploadViewController.swift
+++ Firebase_Practice/PhotoUploadViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 import FirebaseStorage
 
-class PhotoUploadViewController: UIViewController {
-  let storage = Storage.storage()
+class  PhotoUploadViewController: UIViewController {
+  @IBOutlet weak var photoImageView: UIImageView!
+  
+  let storage = Storage.storage().reference()
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
+  @IBAction func pickPhotoBtnTapped(_ sender: Any) {
+    let pickerVC = UIImagePickerController()
+    pickerVC.sourceType = .photoLibrary
+    pickerVC.delegate = self
+    pickerVC.allowsEditing = true
+    present(pickerVC, animated: true, completion: nil)
+  }
   
+  @IBAction func uploadBtnTapped(_ sender: Any) {
+    let randomID = UUID.init().uuidString
+    
+  }
+  
+}
+
+extension PhotoUploadViewController:
+  UIImagePickerControllerDelegate,
+  UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+      return
+    }
+    photoImageView.image = image
+    self.dismiss(animated: true, completion: nil)
+    
+    guard let imageData = image.pngData() else { return }
+    
+    // /Desktop/file.png
+    storage.child("images/file.png").putData(imageData, metadata: nil) { _, error in
+      guard error == nil else {
+        print("Failed to upload")
+        return
+      }
+      self.storage.child("images/file.png").downloadURL { url, error  in
+        guard let url = url, error == nil else { return }
+        let urlString = url.absoluteString
+        print("Download URL: \(urlString)")
+        UserDefaults.standard.set(urlString, forKey: "url")
+      }
+    }
+  }
+  
+  
+  
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true, completion: nil)
+  }
 }
